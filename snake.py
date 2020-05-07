7fd390eedfac17dd8499596570b6e085516a8bc1
Dylan Bolter <dylanbolter@Dylans-MacBook-Pro.local>
2020-05-07 11:34:08 -0500

    changed speed

--- snake.py
+++ snake.py
@@ -8,9 +8,10 @@ player_x = 250
 player_y = 250
 player_height = 20
 player_width = 20
-speed = .5
+speed = 1
 
 is_running = True
+
 
 while is_running:
     for event in pygame.event.get():
@@ -18,18 +19,19 @@ while is_running:
             is_running = False
 
     keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_LEFT]:
-        player_x -= speed
-    if keys[pygame.K_RIGHT]:
-        player_x += speed
-    if keys[pygame.K_UP]:
-        player_y -= speed
-    if keys[pygame.K_DOWN]:
-        player_y += speed
+    if event.type == pygame.KEYDOWN:
+        if keys[pygame.K_LEFT]:
+            player_x -= speed
+        if keys[pygame.K_RIGHT]:
+            player_x += speed
+        if keys[pygame.K_UP]:
+            player_y -= speed
+        if keys[pygame.K_DOWN]:
+            player_y += speed
 
     window.fill((0,0,0))    
     pygame.draw.rect(window, (0,255,0), (player_x, player_y, player_height, player_width))
     pygame.display.update()
+
 
 pygame.quit()
